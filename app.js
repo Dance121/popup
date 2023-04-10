ff6cab189ced50bf01f641f7ccc56ca8431bf7b2
Dance121 <118019813+Dance121@users.noreply.github.com>
2023-04-11 00:40:43 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 const popup = document.querySelector("#popup");
 const closeBtn = document.querySelector("#close");
-let popupShown = false;
-
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    popup.classList.remove("hide-container");
-  }
-  setTimeout(() => {
-    popup.classList.add("hide-container");
+  setTimeout(()=> {
+    if (window.scrollY > 50) {
+      popup.classList.remove("hide-container");
+    }
   }, 3000);
-//   popupShown = true;
 });
 
 closeBtn.addEventListener("click", () => {
-  popup.classList.add("hide-container");
+  popup.style.display = "none";
 });
 
-window.onclick = function(event){
-    if(event.target !== popup){
-        popup.style.display = 'none';
-    }
-}
+window.onclick = function (event) {
+  if (event.target !== popup) {
+    popup.style.display = "none";
+  }
+};
 
 // const classAdd = () => {
 //     if(window.scrollY > 50){
